409edcf3c00ae384ed4302f8838d0edde111009f
Hussain Jafri <your_gitlab_email@example.com>
2026-09-01 17:52:24 +0530

    Add native Terraform import block for existing resource group

--- infra/main.tf
+++ infra/main.tf
@@ -16,3 +16,8 @@ resource "azurerm_resource_group" "main" {
   name     = var.resource_group_name
   location = var.location
 }
+
+import {
+  to = azurerm_resource_group.main
+  id = "/subscriptions/cf623437-cc05-43bb-b63b-dfd5991c17b4/resourceGroups/rg-hussain-enterprise"
+}
