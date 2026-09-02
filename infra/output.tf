55b7b19834a3e4cfa618d00e708b1ab539c30f04
Hussain Jafri <your_gitlab_email@example.com>
2026-09-02 15:16:36 +0530

    Test UI change to trigger build and deployment

--- infra/output.tf
+++ infra/output.tf
@@ -1,7 +1,9 @@
 output "ui_app_name" {
-  value = azurerm_linux_web_app.ui.name
+  value       = azurerm_linux_web_app.ui.name
+  description = "The name of the UI App Service"
 }
 
 output "api_app_name" {
-  value = azurerm_linux_web_app.api.name
+  value       = azurerm_linux_web_app.api.name
+  description = "The name of the API App Service"
 }
