ab68951e4d802cd7ae8cc0be31604bc3cc9aa632
Hussain Jafri <your_gitlab_email@example.com>
2026-09-02 11:54:32 +0530

    Configure Azure Blob Storage remote backend and clean local state

--- api/index.php
+++ api/index.php
@@ -1,3 +1,3 @@
 <?php
 header("Content-Type: application/json");
-echo json_encode(["status" => "fail", "message" => "Enterprise API is running"]);
+echo json_encode(["status" => "pass", "message" => "Enterprise API is running"]);
