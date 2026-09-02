1bf6e55cdf606a8cd531ca6dae49354cb188b8b6
Hussain Jafri <your_gitlab_email@example.com>
2026-09-02 17:27:14 +0530

    Update UI components

--- infra/variables.tf
+++ infra/variables.tf
@@ -7,3 +7,9 @@ variable "resource_group_name" {
   type    = string
   default = "rg-hussain-enterprise"
 }
+
+variable "app_version" {
+  type        = string
+  description = "Global application release version"
+  default     = "1.0.0"
+}
