a38ec6465aad71b78bfb55f064df22fd5efb0051
Hussain Jafri <your_gitlab_email@example.com>
2026-09-02 15:48:22 +0530

    Test UI change to trigger build and deployment

--- api/index.php
+++ api/index.php
@@ -1,3 +1,3 @@
 <?php
 header("Content-Type: application/json");
-echo json_encode(["status" => "pass", "message" => "Enterprise API is running"]);
+echo json_encode(["status" => "110", "message" => "Enterprise API is running"]);
